b631e355e3854f017431a40fb815efbd5031dc18
Frank Song <Frank@wirelessprvnat-172-17-143-146.near.illinois.edu>
2015-05-25 15:14:58 -0500

    mp

--- mp2/testers/tester-1.c
+++ mp2/testers/tester-1.c
@@ -7,7 +7,7 @@ int main()
 	while(1)
 	{
 		count++;
-		int *ptr = malloc(sizeof(int));
+		int *ptr = malloc(1024*1024*1024*1024*sizeof(int));
 			if (ptr == NULL)
 		{
 			printf("Memory failed to allocate!\n");
