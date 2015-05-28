ec8337ea9b7bc56f845126785b3d679b487b4af6
Frank Song <Frank@vpnpool1-130-126-153-204.near.illinois.edu>
2015-05-27 22:51:44 -0500

    mp

--- mp2/alloc.c
+++ mp2/alloc.c
@@ -93,7 +93,6 @@ void *malloc(size_t size)
     
     malloc_size=round_up(size);		//malloc_size is the multiple of 8
     
-    printf("the head pointer is %p\n",head_pointer);
 
     if(head_pointer == NULL)	//first time call,initialization
     {
@@ -172,7 +171,6 @@ void *malloc(size_t size)
     dict* org_head;
     org_head = head_pointer;
 
-    printf("the head pointer is is %p\n",head_pointer);
     //Trasverse the linked list 
     while(1)
     {
@@ -252,6 +250,8 @@ void *malloc(size_t size)
                     //if it is not the last node
                     else
                     {
+                    	
+                printf("this not gonna happen\n");
                     	dict next_head;
                     	dict following_head;
 
