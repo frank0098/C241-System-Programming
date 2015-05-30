b3f46a4b02ab7bcbcc78532d77777a01f8e66ff7
Frank Song <Frank@vpnpool1-130-126-152-76.near.illinois.edu>
2015-05-30 15:06:10 -0500

    mp

--- mp2/alloc.c
+++ mp2/alloc.c
@@ -312,12 +312,14 @@ void *malloc(size_t size)
                     dict previous_head;
                     dict* previous_head_pointer;
                     previous_head_pointer = tmp_dict.prev;
+                    if(previous_head_pointer != NULL)
+                    {
                     previous_head = *previous_head_pointer;
-                    
                     previous_head.size = previous_head.size;
                     previous_head.prev = previous_head.prev;
                     previous_head.next = header_pointer;
                     *previous_head_pointer = previous_head;
+                	}
                     
                     //The header of the next free block
                     dict next_head;
