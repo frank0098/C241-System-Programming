dc49f6adece695b4fc1a176519d701b86b6dc63a
Frank Song <Frank@vpnpool1-130-126-153-191.near.illinois.edu>
2015-05-31 18:12:29 -0500

    mp

--- mp2/alloc.c
+++ mp2/alloc.c
@@ -90,6 +90,7 @@ int round_up(size_t size)
 
 void *head_pointer = NULL;
 void *tail_pointer = NULL;
+void *free_pointer = NULL;
 
 
 void *malloc(size_t size)
@@ -430,6 +431,8 @@ void free(void *ptr)
     //Find next free block
     void* find_next;
     find_next = head_pointer;
+    if(free_pointer <= h_ptr)
+        find_next = free_pointer;
 
     
 
@@ -550,6 +553,7 @@ void free(void *ptr)
     {
     	head_pointer =  h_ptr;
     }
+    free_pointer = h_ptr;
     
 
 }
