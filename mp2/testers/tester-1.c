941291871cd07bbc4a985e650bd7122766ad2cae
Frank Song <Frank@vpnpool1-130-126-152-140.near.illinois.edu>
2015-05-31 02:43:38 -0500

    mp

--- mp2/testers/tester-1.c
+++ mp2/testers/tester-1.c
@@ -5,13 +5,25 @@ int main()
 {
 	int size = 1024;
 	void *ptr = malloc(size);
-	free(ptr);
-	void *ptr2 = malloc(size);
-	free(ptr2);
-	
 	
 	
 
+	
+	
+	if (ptr == NULL)
+	{
+		printf("Memory failed to allocate!\n");
+		return 1;
+	}
+	free(ptr);
+	
+	void *ptr2 = malloc(size);
+	if (ptr2 == NULL)
+	{
+		printf("Memory failed to allocate!\n");
+		return 1;
+	}
+	free(ptr2);
 	
 
 	printf("Memory was allocated, used, and freed!\n");	
