f6e5fdacc6e6f6e688f190f95e86571082121417
Frank Song <Frank@vpnpool1-130-126-152-140.near.illinois.edu>
2015-05-31 00:50:37 -0500

    mp

--- mp2/alloc.c
+++ mp2/alloc.c
@@ -419,7 +419,7 @@ void *malloc(size_t size)
 * */
 void free(void *ptr)
 {
-	getchar();
+	//getchar();
 
 	//printf("\n free \n");
     //"If a null pointer is passed as argument, no action occurs."
