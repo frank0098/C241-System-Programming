9699de8ba6ce5adcac74300fa00fb9e5269fa7b1
Frank Song <Frank@wirelessprvnat-172-17-143-146.near.illinois.edu>
2015-05-31 15:51:45 -0500

    mp

--- mp2/testers/tester-9.c
+++ mp2/testers/tester-9.c
@@ -3,9 +3,9 @@
 
 #define MIN_ALLOC_SIZE 24
 #define MAX_ALLOC_SIZE 1024 * 100
-#define CHANCE_OF_FREE 95
-#define CHANCE_OF_REALLOC 50
-#define TOTAL_ALLOCS 60000
+#define CHANCE_OF_FREE 30
+#define CHANCE_OF_REALLOC 80
+#define TOTAL_ALLOCS 90000
 
 int main()
 {
