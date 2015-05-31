11d893d75b51895a31a1ad662e80f93bb52a71c5
Frank Song <Frank@vpnpool1-130-126-152-140.near.illinois.edu>
2015-05-30 23:17:05 -0500

    mp

--- mp2/testers/tester-1.c
+++ mp2/testers/tester-1.c
@@ -3,11 +3,14 @@
 
 int main()
 {
+	int size = 1024*1024*128;
+	void *ptr = malloc(size);
+	void *ptr1 = realloc(ptr, size/2);
+	void *ptr2 = malloc(size/2);
+	void *ptr3 = realloc(ptr1, size/4);
+	void *ptr3 = malloc( size/4);
 
-	void *ptr = malloc(80000);
-	void *ptr1 = realloc(ptr, 40000);
-	void *ptr2 = malloc(40000);
-	void *ptr3 = realloc(ptr1, 20000);
+	free(ptr4);
 	free(ptr3);
 	free(ptr2);
 	free(ptr1);
