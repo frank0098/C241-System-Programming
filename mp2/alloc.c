e59647a19967fa103d166dd1105b7210efc5c756
Frank Song <Frank@vpnpool1-130-126-153-191.near.illinois.edu>
2015-05-31 18:08:59 -0500

    mp

--- mp2/alloc.c
+++ mp2/alloc.c
@@ -431,8 +431,8 @@ void free(void *ptr)
     //Find next free block
     void* find_next;
     find_next = head_pointer;
-    if(free_pointer <=h_ptr)
-        find_next = free_pointer;
+    // if(free_pointer <=h_ptr)
+    //     find_next = free_pointer;
     
 
     
